da9c9d5e0a6c4574ce9ddff06710bd0ab980a2ab
Naveen Ranwa <naveenrnw@gmail.com>
2025-11-16 14:12:38 +0000

    add multiply method

--- calc.py
+++ calc.py
@@ -3,6 +3,9 @@
 def sutract(a, b):
     retrun a-b
 
+def multiply(a, b):
+    return a*b
+
 def main():
 
     a = input("enter first number: ")
@@ -13,6 +16,9 @@ def main():
         raise("Invalid operation or currently this type of operation is not supported")
     if op == "-":
         ans = subtract(a, b)
+
+    if op == "x":
+        ans = multiply(a, b)
 
     print("Your calucated result is: ", ans)
     # print("you are in main")
